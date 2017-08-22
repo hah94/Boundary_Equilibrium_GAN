55be1cd0b56f1387db48f9ba725fef81a7f485d9
hah94 <frustrating@snu.ac.kr>
2017-08-22 22:16:14 +0900

    error fixed

--- models.py
+++ models.py
@@ -1,11 +1,11 @@
 import tensorflow as tf
 import tensorflow.contrib.slim as slim
 
-def G_conv(z, size=4, channel=3, name='G_conv'): #default for cifar-10 data
+def G_conv(z, channel=3, name='G_conv'): #default for cifar-10 data
 	with slim.arg_scope([slim.fully_connected, slim.conv2d_transpose], activation_fn=tf.nn.relu, normalizer_fn=slim.batch_norm):
 		with slim.arg_scope([slim.conv2d_transpose], padding='SAME', weights_initializer=tf.random_normal_initializer(0, 0.02)):
-			net = slim.fully_connected(z, size * size * 512, scope='fc1')
-			net = tf.reshape(net, (-1, size, size, 512))
+			net = slim.fully_connected(z, 4 * 4 * 512, scope='fc1')
+			net = tf.reshape(net, (-1, 4, 4, 512))
 			net = slim.conv2d_transpose(net, 256, 3, stride=2, scope='deconv1')
 			net = slim.conv2d_transpose(net, 128, 3, stride=2, scope='deconv2')
 			net = slim.conv2d_transpose(net, 64, 3, stride=2, scope='deconv3')
@@ -31,7 +31,7 @@ def D_AE(x, hidden_num=256, reuse=False, name='D_AE'):
 				e = slim.conv2d(e, 64 * 2, scope='conv2')
 				e = slim.conv2d(e, 64 * 4, scope='conv3')
 				e = slim.conv2d(e, 64 * 8, scope='conv4')
-				#e = slim.flatten(e, scope='flatten1')
+				e = slim.flatten(e, scope='flatten1')
 				e = slim.fully_connected(e, hidden_num, scope='fc1')
 		
 		#Decode
